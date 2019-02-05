d827b42c36ab6fe64806a904ce074f0f56933e13
Asaad Budayr <asad_289@hotmail.com>
2019-02-05 08:25:17 -0800

    Feat: Create dummy data to be fetched

---
+++ utils/helpers.js
@@ -0,0 +1,31 @@
+
+const dummyData = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
+
+export function formatResults (results) {
+  return results === null
+    ? dummyData
+    : JSON.parse(results)
+}

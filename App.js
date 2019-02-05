0e788deead9dc57ddc28c5eb361104999a0dd2dd
Asaad Budayr <asad_289@hotmail.com>
2019-02-05 08:38:17 -0800

    Feat: Create Redux store

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import DecksList from "./components/DecksList";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+import { white, lightBlue } from './utils/colors'
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={createStore(reducer)}>
+        <View style={styles.container}>
+          <DecksList />
+        </View>
+      </Provider>
     );
   }
 }
@@ -14,8 +20,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });

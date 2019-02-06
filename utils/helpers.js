d7aea2af556360502fa7002e89e9b2bf317e9aba
Asaad Budayr <asad_289@hotmail.com>
2019-02-06 08:47:30 -0800

    Feat: add function to format new deck

--- utils/helpers.js
+++ utils/helpers.js
@@ -29,3 +29,17 @@ export function formatResults (results) {
     ? dummyData
     : JSON.parse(results)
 }
+
+function generateUID () {
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+export function formatDeck (title) {
+  const id = generateUID()
+  return {
+    [id]:{
+      title: id,
+      questions:[],
+    }
+  }
+}

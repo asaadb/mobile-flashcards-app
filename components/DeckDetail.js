7dc147d37c0b91e54f463b0c9a7cfd0c2ff419fb
Asaad Budayr <asad_289@hotmail.com>
2019-02-10 06:33:39 -0800

    Feat: Add Styling to DeckDetail component

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -7,6 +7,7 @@ import {
   TouchableOpacity
 } from "react-native";
 import { connect } from "react-redux";
+import { gray, purple, white, blue, black, lightGray } from "../utils/colors";
 
 class DeckDetail extends Component {
     render() {
@@ -15,27 +16,64 @@ class DeckDetail extends Component {
       const { entryId } = this.props
       console.log('The entryId:',questions)
       return (
-        <View>
-          <Text>{title}</Text>
-          <Text>{questions ? questions.length: 0} Cards </Text>
-          {questions.length > 0 &&
-          <TouchableOpacity onPress={() => this.props.navigation.navigate(
-              'Quiz',
-              { entryId: entryId}
-            )}>
-            <Text>Start Quiz</Text>
-          </TouchableOpacity>
-          }
-          <TouchableOpacity onPress={() => this.props.navigation.navigate(
-              'AddCard',
-              { entryId: entryId}
-            )}>
-            <Text>Add a Card</Text>
-          </TouchableOpacity>
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.cards}>{questions ? questions.length: 0} Cards </Text>
+          </View>
+          <View>
+            {questions.length > 0 &&
+            <TouchableOpacity style={[styles.button, { backgroundColor: blue }]} onPress={() => this.props.navigation.navigate(
+                'Quiz',
+                { entryId: entryId}
+              )}>
+              <Text style={ styles.buttonText }>Start Quiz</Text>
+            </TouchableOpacity>
+            }
+            <TouchableOpacity style={[styles.button, { backgroundColor: black }]}  onPress={() => this.props.navigation.navigate(
+                'AddCard',
+                { entryId: entryId}
+              )}>
+              <Text style={styles.buttonText}>Add a Card</Text>
+            </TouchableOpacity>
+            </View>
         </View>
       );
     }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'space-around',
+    alignItems:'center',
+    backgroundColor:lightGray,
+  },
+  title: {
+    textAlign:'center',
+    fontSize: 32,
+    margin: 5
+  },
+  cards:{
+    fontSize: 23,
+    color: gray,
+    textAlign:'center',
+     margin: 10
+  },
+  button:{
+    padding: 20,
+    borderRadius: 7,
+    height: 65,
+    margin: 8,
+    paddingLeft:45,
+    paddingRight:45,
+  },
+  buttonText:{
+    fontSize: 22,
+    textAlign: 'center',
+    color: white,
+  },
+})
 function mapStateToProps(decks, { navigation }) {
   const { entryId } = navigation.state.params;
   return {
